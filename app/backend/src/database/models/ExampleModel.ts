8a013395c9871cbae3aae7faf69e2b0c4573b348
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-07 16:02:15 -0300

    refactor(ExampleModel.ts): Arquivo de exemplo totalmente comentado

--- app/backend/src/database/models/ExampleModel.ts
+++ app/backend/src/database/models/ExampleModel.ts
@@ -1,25 +1,25 @@
-import { Model } from 'sequelize';
-import db from '.';
+// import { Model } from 'sequelize';
+// import db from '.';
 // import OtherModel from './OtherModel';
 
-class Example extends Model {
-  // public <campo>!: <tipo>;
-}
+// class Example extends Model {
+//   public <campo>!: <tipo>;
+// }
 
-Example.init({
-  // ... Campos
-}, {
-  // ... Outras configs
-  underscored: true,
-  sequelize: db,
-  // modelName: 'example',
-  timestamps: false,
-});
+// Example.init({
+//   ... Campos
+// }, {
+//   // ... Outras configs
+//   underscored: true,
+//   sequelize: db,
+//   modelName: 'example',
+//   timestamps: false,
+// });
 
-/**
-  * `Workaround` para aplicar as associations em TS: 
-  * Associations 1:N devem ficar em uma das instâncias de modelo
-  * */
+// /**
+//   * `Workaround` para aplicar as associations em TS: 
+//   * Associations 1:N devem ficar em uma das instâncias de modelo
+//   * */
 
 // OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
 // OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
@@ -27,4 +27,4 @@ Example.init({
 // Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
 // Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
 
-export default Example;
+// export default Example;
