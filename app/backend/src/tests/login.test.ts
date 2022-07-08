5c3c38c70017e123507cd7da4c8fc73eb7a18ab7
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-08 20:39:15 -0300

    test(login.test.ts): :bug: Correção geral nos testes "E2E" da rota "/login"

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -19,13 +19,11 @@ describe('Testa a rota "/login"', () => {
     sinon
       .stub(UsersModel, 'findAll')
       .resolves([{
-        username: '',
-        role: '',
-        email: '',
-        password: '',
+        username: 'User',
+        role: 'user',
+        email: 'user@user.com',
+        password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO',
       }] as UsersModel[]);
-    // sinon.stub(UserService, 'generateJwtToken')
-    //   .callsFake(() => '');
   });
 
   after(()=>{
@@ -33,13 +31,14 @@ describe('Testa a rota "/login"', () => {
   });
 
   const loginData = { email: 'user@user.com', password: 'secret_user' };
+  const INVALID_EMAIL_OR_PASSWORD = 'Invalid email or password !';
 
   it(`Uma requisição na rota "POST /login" passando email e password corretos
     retorna um "token" afirmando que o login foi efetuado`, async () => {
     const chaiHttpResponse: Response = await chai.request(app)
       .post('/login').send(loginData);
     expect(chaiHttpResponse.status).to.be.equal(200);
-    expect(chaiHttpResponse.body).to.be.eql({ token: '' }); // ".eql()" serve para comparar objetos;
+    expect(chaiHttpResponse.body).to.have.a.property('token');
   });
 
   it(`Uma requisição na rota "POST /login" passando email correto e password que não existe
@@ -47,19 +46,9 @@ describe('Testa a rota "/login"', () => {
     const requestBody = { ...loginData };
     requestBody.password = '1234567';
     const chaiHttpResponse: Response = await chai.request(app)
-      .post('/login').send(loginData);
-    expect(chaiHttpResponse.status).to.be.equal(200);
-    expect(chaiHttpResponse.body).to.be.eql({ message: '' });
-  });
-
-  it(`Uma requisição na rota "POST /login" passando email que não existe e password correto
-    retorna um objeto que contém o atributo "message" com uma mensagem de erro`, async () => {
-    const requestBody = { ...loginData };
-    requestBody.email = 'notEmail@email.com';
-    const chaiHttpResponse: Response = await chai.request(app)
-      .post('/login').send(loginData);
-    expect(chaiHttpResponse.status).to.be.equal(200);
-    expect(chaiHttpResponse.body).to.be.eql({ message: '' });
+      .post('/login').send(requestBody);
+    expect(chaiHttpResponse.status).to.be.equal(401);
+    expect(chaiHttpResponse.body).to.be.eql({ message: INVALID_EMAIL_OR_PASSWORD }); // ".eql()" serve para comparar objetos;
   });
 
   it(`Uma requisição na rota "POST /login" passando email inválido e password correto
@@ -67,7 +56,7 @@ describe('Testa a rota "/login"', () => {
     const requestBody = { ...loginData };
     requestBody.email = 'useruser.com';
     const chaiHttpResponse: Response = await chai.request(app)
-      .post('/login').send(loginData);
+      .post('/login').send(requestBody);
     expect(chaiHttpResponse.status).to.be.equal(200);
     expect(chaiHttpResponse.body).to.be.eql({ message: '' });
   });
@@ -77,8 +66,33 @@ describe('Testa a rota "/login"', () => {
     const requestBody = { ...loginData };
     requestBody.password = '123456';
     const chaiHttpResponse: Response = await chai.request(app)
-      .post('/login').send(loginData);
+      .post('/login').send(requestBody);
     expect(chaiHttpResponse.status).to.be.equal(200);
     expect(chaiHttpResponse.body).to.be.eql({ message: '' });
+  });
+});
+
+describe('Testa a rota "/login"', () => {
+  before(async () => {
+    sinon
+      .stub(UsersModel, 'findAll')
+      .resolves([]);
+  });
+
+  after(()=>{
+    (UsersModel.findAll as sinon.SinonStub).restore();
+  });
+
+  const loginData = { email: 'user@user.com', password: 'secret_user' };
+  const INVALID_EMAIL_OR_PASSWORD = 'Invalid email or password !';
+
+  it(`Uma requisição na rota "POST /login" passando email que não existe e password correto
+    retorna um objeto que contém o atributo "message" com uma mensagem de erro`, async () => {
+    const requestBody = { ...loginData };
+    requestBody.email = 'notEmail@email.com';
+    const chaiHttpResponse: Response = await chai.request(app)
+      .post('/login').send(requestBody);
+    expect(chaiHttpResponse.status).to.be.equal(401);
+    expect(chaiHttpResponse.body).to.be.eql({ message: INVALID_EMAIL_OR_PASSWORD });
   });
 });
