ca775712d9b1048e4cd8a78214566c0de92ee8f9
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-08 20:40:24 -0300

    feat(UsersService.ts): Validações de existência de usuário no BD e password incorreto implementados

--- app/backend/src/services/UsersService.ts
+++ app/backend/src/services/UsersService.ts
@@ -13,9 +13,12 @@ class UsersService implements IUsersService {
   async login(body: IUserLogin): Promise<IToken | IResponseError> {
     const { password } = body;
     const user: IUser[] | [] = await this.repository.login(body);
+    if (user.length === 0) {
+      return { error: { code: 401, message: 'Invalid email or password !' } };
+    }
     const validPassword = await new Bcrypt().decodeBcryptHash(password, user[0].password);
     if (!validPassword) {
-      return { error: { code: 404, message: 'Invalid email or password !' } };
+      return { error: { code: 401, message: 'Invalid email or password !' } };
     }
     const userToken: string = generateJwtToken(user[0]);
     return { token: userToken };
