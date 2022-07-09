92e2d5424e37a9d05755041ef1d36fff4962899a
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-09 18:52:13 -0300

    feat(constants.ts): Constante "superSecret" (senha secreta dos tokens JWT) adicionada

--- app/backend/src/constants.ts
+++ app/backend/src/constants.ts
@@ -1,3 +1,4 @@
 export const STRING_EMPTY = 'string.empty';
 export const ANY_REQUIRED = 'any.required';
 export const ALL_FIELDS_MUST_BE_FILLED = 'All fields must be filled';
+export const superSecret: string = process.env.JWT_SECRET || 'jwt-super-secret-94074';
