bd39d87a2f3abd11f4c1fcd88dc4d7084a3c6241
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-09 18:53:27 -0300

    refactor(generateJwtToken.ts): Remoção da constante "superSecret" para fazer a importação da mesma do arquivo "constants.ts"

--- app/backend/src/utils/generateJwtToken.ts
+++ app/backend/src/utils/generateJwtToken.ts
@@ -1,9 +1,9 @@
 import * as jwt from 'jsonwebtoken';
 import IUser from '../interfaces/IUser';
+import { superSecret } from '../constants';
 
 const generateJwtToken = (paylod: IUser): string => {
   const validity: string = process.env.TOKEN_VALIDITY || '7d';
-  const superSecret: string = process.env.JWT_SECRET || 'jwt-super-secret-94074';
   const jwtConfig: object = {
     expiresIn: validity,
     algorithm: 'HS256',
