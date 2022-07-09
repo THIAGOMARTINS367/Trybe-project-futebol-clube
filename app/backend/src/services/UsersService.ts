0bdf20da6a36ab8c7937df2f530a71c3620c238e
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-09 15:52:35 -0300

    feat(UsersService.ts): Validações de campos do body, passado no método "login", implementados utilizando a lib "joi"

--- app/backend/src/services/UsersService.ts
+++ app/backend/src/services/UsersService.ts
@@ -1,3 +1,4 @@
+import * as Joi from 'joi';
 import IUsersRepository from '../interfaces/IUsersRepository';
 import IUserLogin from '../interfaces/IUserLogin';
 import IUsersService from '../interfaces/IUsersService';
@@ -8,10 +9,29 @@ import IToken from '../interfaces/IToken';
 import IResponseError from '../interfaces/IResponseError';
 
 class UsersService implements IUsersService {
-  constructor(private repository: IUsersRepository) {}
+  constructor(
+    private repository: IUsersRepository,
+    private joiTypes = Joi.types(),
+  ) {}
+
+  validateLoginFields({ email, password }: IUserLogin) {
+    const { object, string } = this.joiTypes;
+    const { error } = object.keys({
+      email: string.email().not().empty().required().messages({
+        'string.empty': 'All fields must be filled',
+        'any.required': 'All fields must be filled',
+      }),
+      password: string.not().empty().min(7).required(),
+    }).validate({ email, password });
+    return error;
+  }
 
   async login(body: IUserLogin): Promise<IToken | IResponseError> {
     const { password } = body;
+    const validation = this.validateLoginFields(body);
+    if (validation) {
+      return { error: { code: 400, message: validation.message } };
+    }
     const user: IUser[] | [] = await this.repository.login(body);
     if (user.length === 0) {
       return { error: { code: 401, message: 'Invalid email or password !' } };
