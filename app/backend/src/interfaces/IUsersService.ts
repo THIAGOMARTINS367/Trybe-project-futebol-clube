900215e053ddc4cbe8872c5f0a8da21c09760b96
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-09 18:48:14 -0300

    feat(IUsersService.ts): Método "getUserRole" implementaddo na interface "IUsersService"

--- app/backend/src/interfaces/IUsersService.ts
+++ app/backend/src/interfaces/IUsersService.ts
@@ -1,9 +1,11 @@
 import IResponseError from './IResponseError';
 import IToken from './IToken';
+import IUser from './IUser';
 import IUserLogin from './IUserLogin';
 
 interface IUsersService {
   login(body: IUserLogin): Promise<IToken | IResponseError>,
+  getUserRole(userData: IUser): Promise<string | IResponseError>,
 }
 
 export default IUsersService;
