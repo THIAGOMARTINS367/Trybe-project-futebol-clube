ea1a7cbd4325bb9d268ae46d19b5ce74395fedaf
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-11 19:18:13 -0300

    feat(TeamsController.ts): Classe "TeamsController" e método "getAllTeams" criados e implemenatados

---
+++ app/backend/src/controllers/TeamsController.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+import ITeamsService from '../interfaces/ITeamsService';
+
+class TeamsController {
+  constructor(private service: ITeamsService) {}
+
+  async getAllTeams(_req: Request, res: Response, next: NextFunction) {
+    const result = await this.service.getAllTeams();
+    if (Object.keys(result).includes('error')) {
+      return next(result);
+    }
+    res.status(200).json(result);
+  }
+}
+
+export default TeamsController;
