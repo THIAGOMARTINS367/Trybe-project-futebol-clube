8a0d61b44335218883edb03ac27438a94a061db0
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-11 19:48:53 -0300

    feat(ITeamsService.ts): Método "getTeamById" adicionado na interface "ITeamsService"

--- app/backend/src/interfaces/ITeamsService.ts
+++ app/backend/src/interfaces/ITeamsService.ts
@@ -1,7 +1,9 @@
+import IResponseError from './IResponseError';
 import ITeam from './ITeam';
 
 interface ITeamsService {
   getAllTeams(): Promise<ITeam[]>,
+  getTeamById(id: number): Promise<ITeam | IResponseError>,
 }
 
 export default ITeamsService;
