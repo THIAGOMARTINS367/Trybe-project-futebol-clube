4a572127ec2be8a6e348d4394e4a25f5a2c60fad
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-11 19:22:35 -0300

    feat(routes/index.ts): Rota "/teams" implementada

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import * as express from 'express';
 import loginRoutes from './loginRoutes';
+import teamsRouter from './teamsRoutes';
 
 const routes = express.Router();
 
 routes.use('/login', loginRoutes);
+routes.use('/teams', teamsRouter);
 
 export default routes;
