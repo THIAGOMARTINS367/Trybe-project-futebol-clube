841a3fc5b675ff3187c3f191990d05c1839a5fd5
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-11 19:19:47 -0300

    feat(TeamsService.ts): Classe "TeamsService" e método "getAllTeams" criados e implemenatados

---
+++ app/backend/src/services/TeamsService.ts
@@ -0,0 +1,14 @@
+import ITeam from "../interfaces/ITeam";
+import ITeamsRepository from "../interfaces/ITeamsRepository";
+import ITeamsService from "../interfaces/ITeamsService";
+
+class TeamsService implements ITeamsService {
+  constructor(private repository: ITeamsRepository) {}
+
+  async getAllTeams(): Promise<ITeam[]> {
+    const teams = await this.repository.getAllTeams();
+    return teams;
+  }
+}
+
+export default TeamsService;
