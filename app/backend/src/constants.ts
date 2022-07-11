b59118223ff7cf2e18ca108111a52bda1a2b3e34
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-11 17:26:50 -0300

    feat(constants.ts): Constante "validTokenForTests" (Token válido para testes) adicionada

--- app/backend/src/constants.ts
+++ app/backend/src/constants.ts
@@ -2,3 +2,4 @@ export const STRING_EMPTY = 'string.empty';
 export const ANY_REQUIRED = 'any.required';
 export const ALL_FIELDS_MUST_BE_FILLED = 'All fields must be filled';
 export const superSecret: string = process.env.JWT_SECRET || 'jwt-super-secret-94074';
+export const validTokenForTests = process.env.VALID_TOKEN_FOR_TESTS || '';
