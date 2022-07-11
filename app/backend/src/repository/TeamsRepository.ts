f7e676ac8bdfa07cf86531aaf0e5431d6c7a182b
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-11 19:27:59 -0300

    style(TeamsRepository.ts): :art: Alterações feitas para corrigir erros de formatação de código

--- app/backend/src/repository/TeamsRepository.ts
+++ app/backend/src/repository/TeamsRepository.ts
@@ -1,6 +1,6 @@
-import ITeamsRepository from "../interfaces/ITeamsRepository";
-import TeamsModel from "../database/models/TeamsModel";
-import ITeam from "../interfaces/ITeam";
+import ITeamsRepository from '../interfaces/ITeamsRepository';
+import TeamsModel from '../database/models/TeamsModel';
+import ITeam from '../interfaces/ITeam';
 
 class TeamsRepository implements ITeamsRepository {
   constructor(private model = TeamsModel) {}
