765c94e761319a244151f720aad13e8e52573615
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-12 18:33:35 -0300

    feat(matchesRoutes.ts): Rota "POST /matches" criada e implementada

--- app/backend/src/routes/matchesRoutes.ts
+++ app/backend/src/routes/matchesRoutes.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import TokenAuthenticator from '../middlewares/TokenAuthenticator';
 import MatchesController from '../controllers/MatchesController';
 import MatchesRepository from '../repository/MatchesRepository';
 import MatchesService from '../services/MatchesService';
@@ -15,5 +16,15 @@ const entityFactory = () => {
 matchesRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
   entityFactory().getAllMatches(req, res, next);
 });
+
+matchesRouter.post(
+  '/',
+  (req: Request, _res: Response, next: NextFunction) => {
+    new TokenAuthenticator(req, next).validateJwtToken();
+  },
+  (req: Request, res: Response, next: NextFunction) => {
+    entityFactory().addMatch(req, res, next);
+  },
+);
 
 export default matchesRouter;
