700e6381f70911014944856008fce98008abd01e
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-12 19:11:16 -0300

    feat(MatchesService.ts): Método "editMatchProgress", para finalizar um partida no BD, criado e implementado

--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -2,6 +2,7 @@ import IMatchesService from '../interfaces/IMatchesService';
 import IMatchesRepository from '../interfaces/IMatchesRepository';
 import IMatch from '../interfaces/IMatch';
 import INewMatch from '../interfaces/INewMatch';
+import IResponseError from '../interfaces/IResponseError';
 
 class MatchesService implements IMatchesService {
   constructor(private repository: IMatchesRepository) {}
@@ -27,6 +28,14 @@ class MatchesService implements IMatchesService {
   async addMatch(body: INewMatch): Promise<INewMatch> {
     const newMatche: INewMatch = await this.repository.addMatch(body);
     return newMatche;
+  }
+
+  async editMatchProgress(id: number): Promise<{ message: 'Finished' } | IResponseError> {
+    const alteredLines = await this.repository.editMatchProgress(id);
+    if (alteredLines === 0) {
+      return { error: { code: 500, message: `Unable to finish match ${id}` } };
+    }
+    return { message: 'Finished' };
   }
 }
 
