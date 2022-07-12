56f4a5965e6c1d4342b09cc05b1bfce0d6209a90
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-12 16:36:14 -0300

    feat(MatchesService.ts): Classe "MatchesService" e método "getAllMatches" criados e implemenatados

---
+++ app/backend/src/services/MatchesService.ts
@@ -0,0 +1,14 @@
+import IMatchesService from '../interfaces/IMatchesService';
+import IMatchesRepository from '../interfaces/IMatchesRepository';
+import IMatch from '../interfaces/IMatch';
+
+class MatchesService implements IMatchesService{
+  constructor(private repository: IMatchesRepository) {}
+
+  async getAllMatches(): Promise<IMatch[]> {
+    const matches: IMatch[] = await this.repository.getAllMatches();
+    return matches;
+  }
+}
+
+export default MatchesService;
