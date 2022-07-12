af836636e7ece5b5da17ba6119b3bd0cc104911a
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-12 19:10:00 -0300

    feat(MatchesController.ts): Método "editMatchProgress", para finalizar um partida no BD, criado e implementado

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import INewMatch from '../interfaces/INewMatch';
 import IMatch from '../interfaces/IMatch';
 import IMatchesService from '../interfaces/IMatchesService';
+import IResponseError from '../interfaces/IResponseError';
 
 class MatchesController {
   constructor(private service: IMatchesService) {}
@@ -18,6 +19,17 @@ class MatchesController {
   async addMatch(req: Request, res: Response, next: NextFunction) {
     const { body }: { body: INewMatch } = req;
     const result: INewMatch = await this.service.addMatch(body);
+    if (Object.keys(result).includes('error')) {
+      return next(result);
+    }
+    res.status(201).json(result);
+  }
+
+  async editMatchProgress(req: Request, res: Response, next: NextFunction) {
+    const { id } = req.params;
+    const result: {
+      message: 'Finished',
+    } | IResponseError = await this.service.editMatchProgress(Number(id));
     if (Object.keys(result).includes('error')) {
       return next(result);
     }
