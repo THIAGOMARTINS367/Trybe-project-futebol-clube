6fe33f74e6429c5952fa7ffa5cb43a298a7d0712
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-12 16:33:35 -0300

    feat(routes/index.ts): Rota "/matches" implementada

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import * as express from 'express';
 import loginRoutes from './loginRoutes';
+import matchesRouter from './matchesRoutes';
 import teamsRouter from './teamsRoutes';
 
 const routes = express.Router();
 
 routes.use('/login', loginRoutes);
 routes.use('/teams', teamsRouter);
+routes.use('/matches', matchesRouter);
 
 export default routes;
