9574def18c86e9feb139a2ae8aee8b5f19108c5a
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-12 20:01:28 -0300

    feat(IMatchesRepository.ts): Método "getMatchById" adicionado e tipado na interface "IMatchesRepository"

--- app/backend/src/interfaces/IMatchesRepository.ts
+++ app/backend/src/interfaces/IMatchesRepository.ts
@@ -5,6 +5,7 @@ interface IMatchesRepository {
   getAllMatches(queryParameters: { inProgress: boolean }[]): Promise<IMatch[]>,
   addMatch(newUserData: INewMatch): Promise<INewMatch>,
   editMatchProgress(id: number): Promise<number>,
+  getMatchById(id: number): Promise<IMatch | null>,
 }
 
 export default IMatchesRepository;
