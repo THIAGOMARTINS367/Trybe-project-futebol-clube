135bbf1fab9eca1c5fc964cbfde740ada19f6afe
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-12 16:37:38 -0300

    feat(MatchesRepository.ts): Classe "MatchesRepository" e método "getAllMatches" criados e implemenatados

---
+++ app/backend/src/repository/MatchesRepository.ts
@@ -0,0 +1,24 @@
+import TeamsModel from '../database/models/TeamsModel';
+import MatchesModel from '../database/models/MatchesModel';
+import IMatchesRepository from '../interfaces/IMatchesRepository';
+import IMatch from '../interfaces/IMatch';
+
+class MatchesRepository implements IMatchesRepository {
+  constructor(private model = MatchesModel) {}
+
+  async getAllMatches(): Promise<IMatch[]> {
+    const matches: IMatch[] = await this.model.findAll({
+      include: [{
+        model: TeamsModel, as: 'teamHome',
+        attributes: { exclude: ['id'] },
+      },
+      {
+        model: TeamsModel, as: 'teamAway',
+        attributes: { exclude: ['id'] },
+      }]
+    });
+    return matches;
+  };
+}
+
+export default MatchesRepository;
