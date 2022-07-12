43df5af2b2e67090e6bdbdeeb3d1e7cae27e24ba
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-12 18:37:26 -0300

    feat(MatchesService.ts): Método "addMatch", para adicionar nova partida no BD, criado e implementado

--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -1,6 +1,7 @@
 import IMatchesService from '../interfaces/IMatchesService';
 import IMatchesRepository from '../interfaces/IMatchesRepository';
 import IMatch from '../interfaces/IMatch';
+import INewMatch from '../interfaces/INewMatch';
 
 class MatchesService implements IMatchesService {
   constructor(private repository: IMatchesRepository) {}
@@ -21,6 +22,11 @@ class MatchesService implements IMatchesService {
     }
     const matches: IMatch[] = await this.repository.getAllMatches(queryParameters);
     return matches;
+  }
+
+  async addMatch(body: INewMatch): Promise<INewMatch> {
+    const newMatche: INewMatch = await this.repository.addMatch(body);
+    return newMatche;
   }
 }
 
