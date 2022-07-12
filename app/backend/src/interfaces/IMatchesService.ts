d786a5b36b6fe0431fe19d7e5c76088fe1abfcef
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-12 18:36:41 -0300

    feat(IMatchesService.ts): Método "addMatch" adicionado e tipado na interface "IMatchesService"

--- app/backend/src/interfaces/IMatchesService.ts
+++ app/backend/src/interfaces/IMatchesService.ts
@@ -1,7 +1,9 @@
 import IMatch from './IMatch';
+import INewMatch from './INewMatch';
 
 interface IMatchesService {
   getAllMatches(inProgress: string | undefined): Promise<IMatch[]>,
+  addMatch(body: INewMatch): Promise<INewMatch>,
 }
 
 export default IMatchesService;
