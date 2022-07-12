6f0e4b0dbecb80294ed4cb0ac7a5aa22989d7d2b
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-12 19:11:49 -0300

    feat(IMatchesRepository.ts): Método "editMatchProgress" adicionado e tipado na interface "IMatchesRepository"

--- app/backend/src/interfaces/IMatchesRepository.ts
+++ app/backend/src/interfaces/IMatchesRepository.ts
@@ -4,6 +4,7 @@ import INewMatch from './INewMatch';
 interface IMatchesRepository {
   getAllMatches(queryParameters: { inProgress: boolean }[]): Promise<IMatch[]>,
   addMatch(newUserData: INewMatch): Promise<INewMatch>,
+  editMatchProgress(id: number): Promise<number>,
 }
 
 export default IMatchesRepository;
