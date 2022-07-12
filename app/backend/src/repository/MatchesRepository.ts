7a75fcd18af878441e47f29e5edf388ec88d80bf
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-12 17:24:35 -0300

    refactor(MatchesRepository.ts): Parâmetro de busca "queryParameters" implementado na método "findAll" do Sequelize no método "getAllMatches"

--- app/backend/src/repository/MatchesRepository.ts
+++ app/backend/src/repository/MatchesRepository.ts
@@ -1,3 +1,4 @@
+import { Op } from 'sequelize';
 import TeamsModel from '../database/models/TeamsModel';
 import MatchesModel from '../database/models/MatchesModel';
 import IMatchesRepository from '../interfaces/IMatchesRepository';
@@ -6,8 +7,9 @@ import IMatch from '../interfaces/IMatch';
 class MatchesRepository implements IMatchesRepository {
   constructor(private model = MatchesModel) {}
 
-  async getAllMatches(): Promise<IMatch[]> {
+  async getAllMatches(queryParameters: { inProgress: boolean }[]): Promise<IMatch[]> {
     const matches: IMatch[] = await this.model.findAll({
+      where: { [Op.or]: queryParameters },
       include: [{
         model: TeamsModel,
         as: 'teamHome',
