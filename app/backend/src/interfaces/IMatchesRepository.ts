dbcc0031fa45d0fae18c9a713df03a98c0d87e5e
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-12 18:38:11 -0300

    feat(IMatchesRepository.ts): Método "addMatch" adicionado e tipado na interface "IMatchesRepository"

--- app/backend/src/interfaces/IMatchesRepository.ts
+++ app/backend/src/interfaces/IMatchesRepository.ts
@@ -1,7 +1,9 @@
 import IMatch from './IMatch';
+import INewMatch from './INewMatch';
 
 interface IMatchesRepository {
   getAllMatches(queryParameters: { inProgress: boolean }[]): Promise<IMatch[]>,
+  addMatch(newUserData: INewMatch): Promise<INewMatch>,
 }
 
 export default IMatchesRepository;
