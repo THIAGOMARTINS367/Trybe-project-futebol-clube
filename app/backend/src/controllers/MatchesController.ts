30c983d967f7b7cc1ebd577ba9acfd5564d0506f
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-13 15:39:58 -0300

    feat(MatchesController.ts): Método "updateMatch", para editar uma partida no BD, criado e implementado

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -3,6 +3,7 @@ import INewMatch from '../interfaces/INewMatch';
 import IMatch from '../interfaces/IMatch';
 import IMatchesService from '../interfaces/IMatchesService';
 import IResponseError from '../interfaces/IResponseError';
+import IMatchGoals from '../interfaces/IMatchGoals';
 
 class MatchesController {
   constructor(private service: IMatchesService) {}
@@ -30,6 +31,17 @@ class MatchesController {
     const result: {
       message: 'Finished',
     } | IResponseError = await this.service.editMatchProgress(Number(id));
+    if (Object.keys(result).includes('error')) {
+      return next(result);
+    }
+    res.status(200).json(result);
+  }
+
+  async updateMatch(req: Request, res: Response, next: NextFunction) {
+    const { body, params: { id } } = req;
+    const requestBody: IMatchGoals = body;
+    const matchId: string = id;
+    const result: IMatchGoals = await this.service.updateMatch(Number(matchId), requestBody);
     if (Object.keys(result).includes('error')) {
       return next(result);
     }
