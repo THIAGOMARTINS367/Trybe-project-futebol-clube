af4e6e6705a77f847096c61ee59a3a2db0f2aa36
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-13 14:26:29 -0300

    refactor(IMatchesRepository.ts): Método "getMatchById" renomeado para "getTeamById" e tipagem do retorno alterada

--- app/backend/src/interfaces/IMatchesRepository.ts
+++ app/backend/src/interfaces/IMatchesRepository.ts
@@ -1,11 +1,12 @@
 import IMatch from './IMatch';
 import INewMatch from './INewMatch';
+import ITeam from './ITeam';
 
 interface IMatchesRepository {
   getAllMatches(queryParameters: { inProgress: boolean }[]): Promise<IMatch[]>,
   addMatch(newUserData: INewMatch): Promise<INewMatch>,
   editMatchProgress(id: number): Promise<number>,
-  getMatchById(id: number): Promise<IMatch | null>,
+  getTeamById(id: number): Promise<ITeam | null>,
 }
 
 export default IMatchesRepository;
