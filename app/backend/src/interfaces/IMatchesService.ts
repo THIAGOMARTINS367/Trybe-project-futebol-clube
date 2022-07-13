b6fdf5e60ed62354e73d8f075d0e915d169ca38c
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-13 15:40:28 -0300

    feat(IMatchesService.ts): Método "updateMatch" adicionado e tipado na interface "IMatchesService"

--- app/backend/src/interfaces/IMatchesService.ts
+++ app/backend/src/interfaces/IMatchesService.ts
@@ -1,4 +1,5 @@
 import IMatch from './IMatch';
+import IMatchGoals from './IMatchGoals';
 import INewMatch from './INewMatch';
 import IResponseError from './IResponseError';
 
@@ -6,6 +7,7 @@ interface IMatchesService {
   getAllMatches(inProgress: string | undefined): Promise<IMatch[]>,
   addMatch(body: INewMatch): Promise<INewMatch | IResponseError>,
   editMatchProgress(id: number): Promise<{ message: 'Finished' } | IResponseError>,
+  updateMatch(id: number, body: IMatchGoals): Promise<IMatchGoals>,
 }
 
 export default IMatchesService;
