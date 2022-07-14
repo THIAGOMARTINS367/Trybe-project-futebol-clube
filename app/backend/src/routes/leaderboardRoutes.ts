8234d504d44ba69fc3b363cf8ccb39235d9a29bb
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-14 19:00:55 -0300

    feat(leaderboardRoutes.ts): Rota "GET /leaderboard/home" criada e implementada

---
+++ app/backend/src/routes/leaderboardRoutes.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import MatchesController from '../controllers/MatchesController';
+import MatchesRepository from '../repository/MatchesRepository';
+import MatchesService from '../services/MatchesService';
+
+const leaderboardRoutes = Router();
+
+const entityFactory = () => {
+  const repository = new MatchesRepository();
+  const service = new MatchesService(repository);
+  const controller = new MatchesController(service);
+  return controller;
+};
+
+leaderboardRoutes.get('/home', (req: Request, res: Response, next: NextFunction) => {
+  entityFactory().getLeaderboard(req, res, next);
+});
+
+export default leaderboardRoutes;
