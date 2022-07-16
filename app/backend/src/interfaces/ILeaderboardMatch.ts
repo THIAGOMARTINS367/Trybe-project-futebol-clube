0445d1bd9a2d957beb5f43c4db1d6ea7793b0559
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-16 19:21:44 -0300

    refactor(ILeaderboardMatch.ts): Remoção da interface "ILeaderboardMatch" não utilizada

--- app/backend/src/interfaces/ILeaderboardMatch.ts
+++
@@ -1,8 +0,0 @@
-import IMatch from './IMatch';
-
-interface ILeaderboardMatch extends Omit<IMatch, 'teamHome' | 'teamAway'> {
-  homeTeamId: number,
-  teamHomeName: string,
-}
-
-export default ILeaderboardMatch;
