9f01f7ad61252de863ba32eb9c9d78fdebcf45ee
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-18 00:23:29 -0300

    test(matches.test.ts): Novos testes E2E da rota "/matches" implementados

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -5,116 +5,27 @@ import { before } from 'mocha';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import UsersModel from '../database/models/UsersModel';
 
 import { Response } from 'superagent';
+import MatchesModel from '../database/models/MatchesModel';
+import { validTokenForTests } from '../constants';
+import allMatches from './mocks/allMatches';
+import finishedMatches from './mocks/finishedMatches';
+import matchesInProgress from './mocks/matchesInProgress';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-const AllMatches = [
-  {
-    id: 1,
-    homeTeam: 16,
-    homeTeamGoals: 1,
-    awayTeam: 8,
-    awayTeamGoals: 1,
-    inProgress: false,
-    teamHome: {
-      teamName: 'São Paulo',
-    },
-    teamAway: {
-      teamName: 'Grêmio',
-    },
-  },
-  {
-    id: 41,
-    homeTeam: 16,
-    homeTeamGoals: 2,
-    awayTeam: 9,
-    awayTeamGoals: 0,
-    inProgress: true,
-    teamHome: {
-      teamName: 'São Paulo',
-    },
-    teamAway: {
-      teamName: 'Internacional',
-    },
-  },
-];
-
-const matchesInProgress = [
-  {
-    id: 41,
-    homeTeam: 16,
-    homeTeamGoals: 2,
-    awayTeam: 9,
-    awayTeamGoals: 0,
-    inProgress: true,
-    teamHome: {
-      teamName: 'São Paulo',
-    },
-    teamAway: {
-      teamName: 'Internacional',
-    },
-  },
-  {
-    id: 42,
-    homeTeam: 6,
-    homeTeamGoals: 1,
-    awayTeam: 1,
-    awayTeamGoals: 0,
-    inProgress: true,
-    teamHome: {
-      teamName: 'Ferroviária',
-    },
-    teamAway: {
-      teamName: 'Avaí/Kindermann',
-    },
-  },
-];
-
-const finishedMatches = [
-  {
-    id: 1,
-    homeTeam: 16,
-    homeTeamGoals: 1,
-    awayTeam: 8,
-    awayTeamGoals: 1,
-    inProgress: false,
-    teamHome: {
-      teamName: 'São Paulo',
-    },
-    teamAway: {
-      teamName: 'Grêmio',
-    },
-  },
-  {
-    id: 2,
-    homeTeam: 9,
-    homeTeamGoals: 1,
-    awayTeam: 14,
-    awayTeamGoals: 1,
-    inProgress: false,
-    teamHome: {
-      teamName: 'Internacional',
-    },
-    teamAway: {
-      teamName: 'Santos',
-    },
-  },
-];
-
 describe('Testa a rota "GET /matches"', () => {
   before(async () => {
     sinon
-      .stub(UsersModel, 'findAll')
-      .resolves(AllMatches as any);
+      .stub(MatchesModel, 'findAll')
+      .resolves(allMatches as any);
   });
 
   after(()=>{
-    (UsersModel.findAll as sinon.SinonStub).restore();
+    (MatchesModel.findAll as sinon.SinonStub).restore();
   });
 
   it(`Uma requisição na rota "GET /matches" retorna uma
@@ -122,19 +33,19 @@ describe('Testa a rota "GET /matches"', () => {
     const chaiHttpResponse: Response = await chai.request(app)
       .get('/matches');
     expect(chaiHttpResponse.status).to.be.equal(200);
-    expect(chaiHttpResponse.body).to.be.eql(AllMatches);
+    expect(chaiHttpResponse.body).to.be.eql(allMatches);
   });  
 });
 
 describe('Testa a rota "GET /matches"', () => {
   before(async () => {
     sinon
-      .stub(UsersModel, 'findAll')
+      .stub(MatchesModel, 'findAll')
       .resolves(matchesInProgress as any);
   });
 
   after(()=>{
-    (UsersModel.findAll as sinon.SinonStub).restore();
+    (MatchesModel.findAll as sinon.SinonStub).restore();
   });
 
   it(`Uma requisição na rota "GET /matches" filtrando as partidas para "em andamento"
@@ -150,12 +61,12 @@ describe('Testa a rota "GET /matches"', () => {
 describe('Testa a rota "GET /matches"', () => {
   before(async () => {
     sinon
-      .stub(UsersModel, 'findAll')
-      .resolves(finishedMatches as any);
+      .stub(MatchesModel, 'findAll')
+      .resolves(finishedMatches as unknown as MatchesModel[]);
   });
 
   after(()=>{
-    (UsersModel.findAll as sinon.SinonStub).restore();
+    (MatchesModel.findAll as sinon.SinonStub).restore();
   });
 
   it(`Uma requisição na rota "GET /matches" filtrando as partidas para "finalizadas"
@@ -165,5 +76,108 @@ describe('Testa a rota "GET /matches"', () => {
       .get('/matches?inProgress=false');
     expect(chaiHttpResponse.status).to.be.equal(200);
     expect(chaiHttpResponse.body).to.be.eql(finishedMatches);
+  });
+});
+
+describe('Testa a rota "POST /matches"', () => {
+  const matchCreated = {
+    id: 49,
+    homeTeam: 16,
+    awayTeam: 8,
+    homeTeamGoals: 2,
+    awayTeamGoals: 2,
+    inProgress: true,
+  }
+  const requestBody = {
+    homeTeam: 16,
+    awayTeam: 8,
+    homeTeamGoals: 2,
+    awayTeamGoals: 2,
+  }
+  before(async () => {
+    sinon
+      .stub(MatchesModel, 'create')
+      .resolves(matchCreated as unknown as MatchesModel);
+  });
+
+  after(()=>{
+    (MatchesModel.create as sinon.SinonStub).restore();
+  });
+
+  it(`Uma requisição na rota "POST /matches" passando as informações corretas
+  de uma partida no body da requisição e um token válido retorna os dados
+  da partida recém-criada`, async () => {
+    const chaiHttpResponse: Response = await chai.request(app)
+      .post('/matches').set('Authorization', validTokenForTests).send(requestBody);
+    expect(chaiHttpResponse.body).to.be.eql(matchCreated);
+    expect(chaiHttpResponse.status).to.be.equal(201);
+  });
+  it(`Uma requisição na rota "POST /matches" passando as informações corretas
+  de uma partida no body da requisição e um token inválido retorna uma mensagem de erro`, async () => {
+    const chaiHttpResponse: Response = await chai.request(app)
+      .post('/matches').set('Authorization', `${validTokenForTests}E`).send(requestBody);
+    expect(chaiHttpResponse.body).to.be.eql({ message: 'Token must be a valid token' });
+    expect(chaiHttpResponse.status).to.be.equal(401);
+  });
+  it(`Uma requisição na rota "POST /matches" passando as informações corretas
+  de uma partida no body da requisição mas, sem um token no header "Authorization"
+  retorna uma mensagem de erro`, async () => {
+    const chaiHttpResponse: Response = await chai.request(app)
+      .post('/matches').set('Authorization', '').send(requestBody);
+    expect(chaiHttpResponse.body).to.be.eql({ message: 'Token not found' });
+    expect(chaiHttpResponse.status).to.be.equal(401);
+  });
+});
+
+describe('Testa a rota "PATCH /matches/:id/finish"', () => {
+  before(async () => {
+    sinon
+      .stub(MatchesModel, 'update')
+      .resolves([1, MatchesModel] as unknown as [number, MatchesModel[]]);
+  });
+
+  after(()=>{
+    (MatchesModel.update as sinon.SinonStub).restore();
+  });
+
+  it(`Uma requisição na rota "PATCH /matches/48/finish" passando o id
+  de uma partida no parâmetro da rota retorna a mensagem "Finished"
+  e status correto`, async () => {
+    const chaiHttpResponse: Response = await chai.request(app)
+      .patch('/matches/48/finish');
+    expect(chaiHttpResponse.body).to.be.eql({ message: 'Finished' });
+    expect(chaiHttpResponse.status).to.be.equal(200);
+  });
+});
+
+describe('Testa a rota "PATCH /matches/:id"', () => {
+  before(async () => {
+    sinon
+      .stub(MatchesModel, 'update')
+      .resolves([1, MatchesModel] as unknown as [number, MatchesModel[]]);
+  });
+
+  after(()=>{
+    (MatchesModel.update as sinon.SinonStub).restore();
+  });
+
+  const requestBody = {
+    homeTeamGoals: 3,
+    awayTeamGoals: 1,
+  }
+
+  const response = {
+    id: 48,
+    homeTeamGoals: 3,
+    awayTeamGoals: 1,
+  }
+
+  it(`Uma requisição na rota "PATCH /matches/48" passando o id
+  de uma partida no parâmetro da rota e os dados corretos no corpo da requisição
+  retorna o id e os dados editados da partida`, async () => {
+    const chaiHttpResponse: Response = await chai.request(app)
+      .patch('/matches/48').send(requestBody);
+    expect(chaiHttpResponse.body).to.be.eql(response);
+    expect(chaiHttpResponse.status).to.be.equal(200);
   });
 });
